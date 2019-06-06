11a748290e55ba65b87464d28fcd4bb921a481e2
Hearues Zueke <hziko314@gmail.com>
2019-06-06 19:34:18 +0200

    add first part, finished so far

---
+++ Polynomial.h
@@ -0,0 +1,30 @@
+//
+// Created by doublepmcl on 06.06.19.
+//
+
+#ifndef SIMPLEVECTORPRINTS_POLYNOMIAL_H
+#define SIMPLEVECTORPRINTS_POLYNOMIAL_H
+
+
+#include <iostream>
+#include <vector>
+
+#include "Polynome.h"
+
+using namespace std;
+
+class Polynomial {
+private:
+    vector<Polynome> _polynomes;
+public:
+    Polynomial(const vector<Polynome>& polynomes);
+    Polynomial(const Polynomial& obj);
+    virtual ~Polynomial();
+
+    void multiplyPolynomialItself();
+
+    friend ostream& operator<<(ostream& os, const Polynomial& obj);
+};
+
+
+#endif //SIMPLEVECTORPRINTS_POLYNOMIAL_H
